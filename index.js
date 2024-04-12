2e6b3477a4ee3cef93d76a918d55a664bae96334
+ <+>
2024-04-12 17:25:22 +0300

    Code working

--- index.js
+++ index.js
@@ -1,9 +1,28 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  //1.convert the word to an array
+  word = word.toLowerCase()
+  const reversedWord=[]
+
+  //2.Loop through the original word in reverse order adding each character to reversedArray
+  for(let i=word.length - 1; i >= 0; i--){
+    reversedWord.push(word[i]);
+  }
+  // 3. Join the characters in the reversedWord array to form a string
+  const joinedReversedWord = reversedWord.join('');
+
+  // 4. Compare the original word with the reversed word
+  return word === joinedReversedWord;
 }
 
 /* 
   Add your pseudocode here
+  Prompt user for input{
+    1.Create a blank array to hold our reversed string
+    2.Loop through the original word in reverse order adding each character to the reversedArray
+    3.Join the characters in a reversed array to form a string 
+    4.Return true is they are the same and false if they are not
+  }
 */
 
 /*
